2e2d69613ece4c3e10ad31f8af53d91ac0b9badd
sabersss618-sketch <sabersss618@gmail.com>
2026-07-21 14:18:29 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Stalking On EyeSMP",     // this is what people actually see
+        state: "Stalking On Transcended Network",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
